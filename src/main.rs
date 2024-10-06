88c99fc905d2a78a2193305f6128f793ece190a3
Joseph Wasden <jpwasden93@gmail.com>
2024-10-06 14:32:29 +0100

    Added regex matching

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,45 @@
 use std::env;
 
 use dotenv::dotenv;
+use regex::Regex;
 use serenity::{async_trait, Client};
 use serenity::all::{GatewayIntents, Message};
 use serenity::client::EventHandler;
 use serenity::prelude::*;
 
-struct Handler;
+struct Handler {
+    card_regex: Regex
+}
+
+impl Handler {
+    fn new() -> Self {
+        let card_regex = Regex::new(r"\[[a-zA-Z]+]").expect("Invalid regex");
+
+        Handler {
+            card_regex
+        }
+    }
+
+    async fn send(&self, content: &str, msg: &Message, ctx: &Context) {
+        if let Err(why) = msg.channel_id.say(&ctx.http, content).await {
+            println!("Error sending message - {why:?}")
+        }
+    }
+}
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("Error sending message - {why:?}")
+        if msg.author.id == ctx.cache.current_user().id {
+            return;
+        } else if msg.content == "!ping" {
+            self.send("Pong!", &msg, &ctx).await
+        } else {
+            if let Some(captures) = self.card_regex.captures(&msg.content) {
+                for capture in captures.iter() {
+                    let Some(card) = capture else { continue };
+                    self.send(card.as_str(), &msg, &ctx).await
+                }
             }
         }
     }
@@ -28,8 +54,10 @@ async fn main() {
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT;
 
+    let handler = Handler::new();
+
     let mut client = Client::builder(&token, intents)
-        .event_handler(Handler)
+        .event_handler(handler)
         .await
         .expect("Error creating client");
 
