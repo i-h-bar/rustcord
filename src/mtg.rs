0d31b2cba695df2595b9700176a1e433a5917d22
Joseph Wasden <jpwasden93@gmail.com>
2024-10-07 23:10:29 +0100

    Added in join of futures in place of looping through matches

--- src/mtg.rs
+++ src/mtg.rs
@@ -1,5 +1,7 @@
+use regex::Match;
 use serde::Deserialize;
 use serenity::all::Message;
+use serenity::futures::future::join_all;
 use serenity::prelude::*;
 
 use crate::mtg::response::CardResponse;
@@ -11,48 +13,59 @@ const SCRYFALL: &str = "https://api.scryfall.com/cards/named?fuzzy=";
 
 impl Handler {
     pub async fn find_cards(&self, msg: &Message, ctx: &Context) {
-        for capture in self.card_regex.captures_iter(&msg.content) {
-            let Some(name) = capture.get(1) else {
-                continue;
-            };
-            println!("Searching scryfall for \"{}\"", name.as_str());
-            let response = self
-                .http_client
-                .get(format!("{}{}", SCRYFALL, name.as_str().replace(" ", "+")))
-                .send()
-                .await
-                .expect("Failed request");
-
-            let card = if response.status().is_success() {
-                match response.json::<CardResponse>().await {
-                    Ok(response) => response,
-                    Err(why) => {
-                        println!("Error getting card from scryfall - {why:?}");
-                        continue;
-                    }
+        let futures: Vec<_> = self
+            .card_regex
+            .captures_iter(&msg.content)
+            .filter_map(| capture| {
+                if let Some(name) = capture.get(1) {
+                    Some(self.find_card(name.as_str(), &msg, &ctx))
+                } else {
+                    None
                 }
-            } else {
-                continue;
-            };
+            })
+            .collect();
 
-            println!(
-                "Matched with - \"{}\". Now searching for image...",
-                card.name
-            );
+        join_all(futures).await;
+    }
 
-            let Ok(image) = self
-                .http_client
-                .get(card.image_uris.png)
-                .send()
-                .await
-                .expect("failed request")
-                .bytes()
-                .await
+    async fn find_card(&self, name: &str, msg: &Message, ctx: &Context)  {
+        println!("Searching scryfall for \"{}\"", name);
+        let response = self
+            .http_client
+            .get(format!("{}{}", SCRYFALL, name.replace(" ", "+")))
+            .send()
+            .await
+            .expect("Failed request");
+
+        let card = if response.status().is_success() {
+            match response.json::<CardResponse>().await {
+                Ok(response) => response,
+                Err(why) => {
+                    println!("Error getting card from scryfall - {why:?}");
+                    return;
+                }
+            }
+        } else {
+            return;
+        };
+
+        println!(
+            "Matched with - \"{}\". Now searching for image...",
+            card.name
+        );
+
+        let Ok(image) = self
+            .http_client
+            .get(card.image_uris.png)
+            .send()
+            .await
+            .expect("failed request")
+            .bytes()
+            .await
             else {
-                continue;
+                return;
             };
-            println!("Image found for - \"{}\".", card.name);
-            utils::send_image(image, format!("{}.png", card.name), &msg, &ctx).await;
-        }
+        println!("Image found for - \"{}\".", card.name);
+        utils::send_image(image, format!("{}.png", card.name), &msg, &ctx).await;
     }
 }
