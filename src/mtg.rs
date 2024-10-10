cedc1d082fe34de6e59bd30ae9423e451594c924
Joseph Wasden <jpwasden93@gmail.com>
2024-10-10 07:38:13 +0100

    Messy update to use sqlx correctly

--- src/mtg.rs
+++ src/mtg.rs
@@ -79,16 +79,44 @@ impl Handler {
         image: Bytes,
     ) {
         let image_id = Uuid::new_v4().to_string();
-        let encoded = BASE64_STANDARD.encode(&image.to_vec());
+        let image_vec = image.to_vec();
+        let image_insert = sqlx::query(r#"INSERT INTO images (id, png) values (uuid($1), $2)"#)
+            .bind(&image_id)
+            .bind(&image_vec);
+
+        let set_insert = sqlx::query(r#"INSERT INTO sets (id, name, code) values (uuid($1), $2, $3) ON CONFLICT DO NOTHING"#)
+            .bind(&card.set_id)
+            .bind(&card.set_name)
+            .bind(&card.set);
+
+        let card_insert = sqlx::query(r#"INSERT INTO cards (id, name, flavour_text, set_id, image_id, artist) values (uuid($1), $2, $3, uuid($4), uuid($5), $6)"#)
+            .bind(&card.id)
+            .bind(&card.name)
+            .bind(&card.flavor_text)
+            .bind(&card.set_id)
+            .bind(&image_id)
+            .bind(&card.artist);
+
         if let Err(why) = self
             .pg_pool
-            .execute(sqlx::query(&format!(
-                "INSERT INTO images values (uuid('{}'), decode('{}', 'base64'))",
-                image_id, encoded
-            )))
+            .execute(image_insert)
             .await
         {
             println!("Failed images insert - {why}")
+        };
+        if let Err(why) = self
+            .pg_pool
+            .execute(set_insert)
+            .await
+        {
+            println!("Failed set insert - {why}")
+        };
+        if let Err(why) = self
+            .pg_pool
+            .execute(card_insert)
+            .await
+        {
+            println!("Failed card insert - {why}")
         };
     }
 }
