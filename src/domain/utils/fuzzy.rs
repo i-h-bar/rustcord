9ab388f8762495b6026b3f1f2be7f25a2307de85
Joseph Wasden <jpwasden93@gmail.com>
2025-09-03 21:16:41 +0100

    Adding bitmask jaro-winkler implementation

--- src/domain/utils/fuzzy.rs
+++ src/domain/utils/fuzzy.rs
@@ -36,37 +36,34 @@ impl ToChars for Box<str> {
 }
 
 
-fn experimental_jw<A: PartialEq<B> + ToChars, B: ToChars>(a: &A, b: &B) -> f32 {
-    let mut a_chars = a.to_chars().collect::<Vec<_>>();
-    let mut b_chars = b.to_chars().collect::<Vec<_>>();
-
+#[allow(clippy::cast_precision_loss)]
+pub fn jaro_winkler_bitmask(a: &str, b: &str) -> Option<f32> {
+    let a_chars: Vec<char> = a.chars().collect();
+    let b_chars: Vec<char> = b.chars().collect();
     let len_a = a_chars.len();
     let len_b = b_chars.len();
 
-    if len_a == 0 || len_b == 0 {
-        return 0.0;
+    if len_a > 64 || len_b > 64 {
+        return None;
     }
 
     if a == b {
-        return 1.0;
+        return Some(1.0);
     }
 
     let max_dist = (len_a.max(len_b) / 2).saturating_sub(1);
     let mut matches = 0.0;
-    let mut transpositions = 0.0;
-    let mut hash_a = vec![false; len_a];
-    let mut hash_b = vec![false; len_b];
-
-
+    let mut hash_a: u64 = 0;
+    let mut hash_b: u64 = 0;
 
     for i in 0..len_a {
         let start = i.saturating_sub(max_dist);
         let end = (i + max_dist + 1).min(len_b);
 
         for j in start..end {
-            if !hash_b[j] && a_chars[i] == b_chars[j] {
-                hash_a[i] = true;
-                hash_b[j] = true;
+            if (hash_b & (1 << j)) == 0 && a_chars[i] == b_chars[j] {
+                hash_a |= 1 << i;
+                hash_b |= 1 << j;
                 matches += 1.0;
                 break;
             }
@@ -74,26 +71,21 @@ fn experimental_jw<A: PartialEq<B> + ToChars, B: ToChars>(a: &A, b: &B) -> f32 {
     }
 
     if matches == 0.0 {
-        return 0.0;
+        return Some(0.0);
     }
 
-    let mut a_match_chars: Vec<char> = Vec::with_capacity(matches as usize);
-    let mut b_match_chars: Vec<char> = Vec::with_capacity(matches as usize);
+    let mut transpositions = 0.0;
+    let mut b_ptr = 0;
 
     for i in 0..len_a {
-        if hash_a[i] {
-            a_match_chars.push(a_chars[i]);
-        }
-    }
-    for i in 0..len_b {
-        if hash_b[i] {
-            b_match_chars.push(b_chars[i]);
-        }
-    }
-
-    for i in 0..(matches as usize) {
-        if a_match_chars[i] != b_match_chars[i] {
-            transpositions += 1.0;
+        if (hash_a & (1 << i)) != 0 {
+            while (hash_b & (1 << b_ptr)) == 0 {
+                b_ptr += 1;
+            }
+            if a_chars[i] != b_chars[b_ptr] {
+                transpositions += 1.0;
+            }
+            b_ptr += 1;
         }
     }
 
@@ -103,9 +95,8 @@ fn experimental_jw<A: PartialEq<B> + ToChars, B: ToChars>(a: &A, b: &B) -> f32 {
             (matches - transpositions / 2.0) / matches
     );
 
-    // Winkler modification
     let mut prefix_len = 0;
-    for (c1, c2) in a_chars.into_iter().zip(b_chars) {
+    for (c1, c2) in a.chars().zip(b.chars()) {
         if c1 == c2 {
             prefix_len += 1;
         } else {
@@ -113,10 +104,10 @@ fn experimental_jw<A: PartialEq<B> + ToChars, B: ToChars>(a: &A, b: &B) -> f32 {
         }
     }
 
-    let prefix_len = prefix_len.min(4) as f32;
+    let prefix_len = (prefix_len as usize).min(4) as f32;
     let scaling_factor = 0.1;
 
-    jaro_similarity + (prefix_len * scaling_factor * (1.0 - jaro_similarity))
+    Some(jaro_similarity + (prefix_len * scaling_factor * (1.0 - jaro_similarity)))
 }
 
 #[allow(clippy::cast_precision_loss)]
@@ -197,6 +188,17 @@ pub fn winkliest_match<
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_jaro_winkler_bitmasl() {
+        let a = "CRATE";
+        let b = "TRACE";
+
+        let answer = jaro_winkler_bitmask(a, b);
+        assert!(answer.is_some());
+
+        assert_eq!(answer.unwrap(), 0.73333335)
+    }
 
     #[test]
     fn test_jaro_winkler() {
