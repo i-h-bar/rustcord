a100e2a00460f8dd9c9ef0094565ed1442d11c54
Joe Wasden <jpwaden93@gmail.com>
2026-05-20 12:06:56 +0100

    Enriched the land description with emoji

--- packages/bot/src/adapters/drivers/discord/utils/description.rs
+++ packages/bot/src/adapters/drivers/discord/utils/description.rs
@@ -1,6 +1,14 @@
 use contracts::card::Card;
 use contracts::set::Set;
 
+const LAND_TYPE_MAP: [(&str, &str); 5] = [
+    ("Mountain", "⛰️"),
+    ("Plains", "☀️"),
+    ("Swamp", "💀"),
+    ("Island", "💧"),
+    ("Forest", "🌲"),
+];
+
 pub fn create_card_description(card: &Card) -> String {
     let stat = match (
         card.power(),
@@ -14,7 +22,35 @@ pub fn create_card_description(card: &Card) -> String {
         _ => String::new(),
     };
 
-    format!("{} • {}{}", card.type_line(), card.mana_cost(), stat)
+    let cost = match card.mana_cost() {
+        "" => {
+            let type_line = card.type_line();
+            if type_line.starts_with("Land")
+                || type_line.starts_with("Basic")
+                || type_line.starts_with("Snow")
+            {
+                let mut base = String::from(" • ");
+                let mut contained_type = false;
+                for (land_type, emoji) in LAND_TYPE_MAP {
+                    if type_line.contains(land_type) {
+                        contained_type = true;
+                        base.push_str(emoji);
+                    }
+                }
+
+                if !contained_type {
+                    base.push('🧭');
+                }
+
+                base
+            } else {
+                String::new()
+            }
+        }
+        cost => format!(" • {cost}"),
+    };
+
+    format!("{}{}{}", card.type_line(), cost, stat)
 }
 
 pub fn create_set_description(set: &Set) -> String {
