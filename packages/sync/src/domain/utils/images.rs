43b8dea76fb0affcc1a3331a477d02673882c17f
Joe Wasden <jpwaden93@gmail.com>
2026-06-19 14:36:21 +0100

    Removed the semophore from image saving now rate limiting is in Scryfall request

--- packages/sync/src/domain/utils/images.rs
+++ packages/sync/src/domain/utils/images.rs
@@ -2,40 +2,22 @@ use crate::ports::image_store::ImageStore;
 use crate::ports::source::CardSource;
 use crate::ports::storage::CardInfo;
 use futures::future;
-use std::sync::Arc;
-use tokio::sync::Semaphore;
 
 async fn save_card(
     card: &CardInfo,
     image_store: &impl ImageStore,
     source: &impl CardSource,
-    sem: Arc<Semaphore>,
 ) {
-    if !image_store.card_image_exists(card).await {
-        let image = {
-            let Ok(_guard) = sem.acquire().await else {
-                log::error!("Poisoned semaphore");
-                return;
-            };
-            source.get_image(card).await
-        };
-
-        if let Some(image) = image {
-            image_store.save_image(image).await;
-        }
+    if !image_store.card_image_exists(card).await
+        && let Some(image) = source.get_image(card).await
+    {
+        image_store.save_image(image).await;
     }
 
-    if !image_store.card_illustration_exists(card).await {
-        let illustration = {
-            let Ok(_guard) = sem.acquire().await else {
-                log::error!("Poisoned semaphore");
-                return;
-            };
-            source.get_illustration(card).await
-        };
-        if let Some(illustration) = illustration {
-            image_store.save_illustration(illustration).await;
-        }
+    if !image_store.card_illustration_exists(card).await
+        && let Some(illustration) = source.get_illustration(card).await
+    {
+        image_store.save_illustration(illustration).await;
     }
 }
 
@@ -45,13 +27,10 @@ pub async fn save_images(
     source: &impl CardSource,
 ) {
     log::info!("Saving {} images", cards.len());
-
-    let sem = Arc::new(Semaphore::new(5));
-
     future::join_all(
         cards
             .iter()
-            .map(|card| save_card(card, image_store, source, Arc::clone(&sem))),
+            .map(|card| save_card(card, image_store, source)),
     )
     .await;
 }
